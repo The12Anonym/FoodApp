4822c77adbf045de11003ce6f34a0752006d30f4
The12Anonym <lucbaertschi@gmail.com>
2016-10-06 14:00:02 +0200

    inkl search

--- App/www/js/controllers.js
+++ App/www/js/controllers.js
@@ -5,21 +5,20 @@ angular.module('starter.controllers', [])
         })
 
         .controller('RezepteCtrl', function ($scope, rezepte, $stateParams) {
-
-
-            $scope.Rezepte = rezepte.all();
-            $scope.rezept = rezepte.get($stateParams.rezeptId);
-            $scope.Kategorien = rezepte.getKat();
+            $scope.Rezepte = rezepte.all(); // -- Rezepte Array
+            $scope.rezept = rezepte.get($stateParams.rezeptId); // -- Rezept als Objekt
+            $scope.Kategorien = rezepte.getKat(); // -- Kategorie Array
+            $scope.query = {name: ""}; // -- Filterfunktion
+            $scope.sort = {predicate: "name", reverse: false}; // -- Filterfunktion
         })
 
         .controller('AddCtrl', function ($scope) {
 
         })
 
-        .controller('SearchCtrl', function ($scope) {
-            $scope.search = function () {
-            Search($scope);
-        };
+        .controller("FilterCtrl", function FilterCtrl($scope) {
+            $scope.query = {name: ""};
+            $scope.sort = {predicate: "name", reverse: false};            
         });
 
         
