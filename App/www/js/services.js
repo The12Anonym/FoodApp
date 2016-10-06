04abad5063ed475aaa2e2feaeaca16db65a31370
The12Anonym <lucbaertschi@gmail.com>
2016-10-06 09:27:33 +0200

    details finisch

--- App/www/js/services.js
+++ App/www/js/services.js
@@ -3,8 +3,14 @@ angular.module('starter.services', [])
         .factory('rezepte', function () {
             // Might use a resource here that returns a JSON array
 
-            var Lasagne = new Gericht('0', 'Lasagne', 'teig', 'lange', 'gut', 'ca 420 min','2.jpg', '2');
+            var Lasagne = new Gericht('0', 'Lasagne', '500 g Teug; 1kg zocker', 'teig einlegen; sauce draufschmieren; kochen', 'gut', 'ca 30 min','2.jpg', '2');
             var rezepte = GetGerichte();
+            for (var i = 0; i < rezepte.length; i++)
+            {
+                 rezepte[i].zutaten = rezepte[i].zutaten.split(';');
+                 rezepte[i].zubereitung = rezepte[i].zubereitung.split(';'); 
+            }
+            
 
             return {
                 all: function () {
