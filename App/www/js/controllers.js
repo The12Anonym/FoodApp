00a2d799a224bb9ee8d3288e523f4dfd527c199a
The12Anonym <lucbaertschi@gmail.com>
2016-10-06 15:04:17 +0200

    with camera Ctrl

--- App/www/js/controllers.js
+++ App/www/js/controllers.js
@@ -18,7 +18,34 @@ angular.module('starter.controllers', [])
 
         .controller("FilterCtrl", function FilterCtrl($scope) {
             $scope.query = {name: ""};
-            $scope.sort = {predicate: "name", reverse: false};            
+            $scope.sort = {predicate: "name", reverse: false};
+        })
+
+        .controller("ExampleController", function ($scope, $cordovaCamera) {
+
+            $scope.takePicture = function () {
+                var options = {
+                    quality: 75,
+                    destinationType: Camera.DestinationType.DATA_URL,
+                    sourceType: Camera.PictureSourceType.CAMERA,
+                    allowEdit: true,
+                    encodingType: Camera.EncodingType.JPEG,
+                    targetWidth: 300,
+                    targetHeight: 300,
+                    popoverOptions: CameraPopoverOptions,
+                    saveToPhotoAlbum: false
+                };
+
+                $cordovaCamera.getPicture(options).then(function (imageData) {
+                    $scope.imgURI = "data:image/jpeg;base64," + imageData;
+                }, function (err) {
+                    // An error occured. Show a message to the user
+                });
+            }
+
         });
+;
+
+
 
         
--- App/www/js/services.js
+++ App/www/js/services.js
@@ -6,7 +6,7 @@ angular.module('starter.services', [])
             var Lasagne = new Gericht('0', 'Lasagne', '500 g Teug; 1kg zocker', 'teig einlegen; sauce draufschmieren; kochen', 'gut', 'ca 30 min','2.jpg', '0');
             var Pizza = new Gericht('1', 'Pizza','Teig; Tomaten; Salami; balsamico' , 'pizza belegen; sauce draufschmieren; 10 min im Backofen bei 200 grad backen', 'italienische Pizza', 'ca 30 min','3.jpg', '0');
             var Spaghetti = new Gericht('2', 'Spaghetti', 'Wasser; nudeln', 'wasser aufkochen; Nudeln ca. 15 min kochen lassen; nach Wunsch salzen etc.', 'spezielle Nudeln', 'ca 20 min','1.jpg', '0');
-            var dlace = new Gericht('3', 'Eis', ' 1l Milch; 50g Zucker', 'Milch aufkochen; Zocker in Milch giessen;  und fertig ist das eis', 'hausgemachte Eiscrème', 'ca 40 min','4.jpg', '1');
+            var glace = new Gericht('3', 'Eis', ' 1l Milch; 50g Zucker', 'Milch aufkochen; Zocker in Milch giessen;  und fertig ist das eis', 'hausgemachte Eiscrème', 'ca 40 min','4.jpg', '1');
             var rezepte = GetGerichte();
             for (var i = 0; i < rezepte.length; i++)
             {
