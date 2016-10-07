d42c295e478e5b8c71808ff7cb9a045224e0f50c
The12Anonym <lucbaertschi@gmail.com>
2016-10-07 13:43:13 +0200

    clean new pic

--- App/www/js/addRezept.js
+++ App/www/js/addRezept.js
@@ -3,10 +3,25 @@ function addR($scope)
     var e = document.getElementById("KatAuswahl");
     var KategorieIn = e.options[e.selectedIndex].value;
     var Gerichtlist = GetGerichte();
-    var GerichtNew = new Gericht(Gerichtlist.length, $scope.NameIn, $scope.ZutatenIn, $scope.ZubereitungIn, $scope.BeschreibungIn, $scope.DauerIn, ImgDATAuri, KategorieIn);
+    var zutaten = $scope.ZutatenIn;
+    var zubereitung = $scope.ZubereitungIn;
+    var strArr = [zutaten, zubereitung];
+    var arr = StrToArr(strArr);
+    var GerichtNew = new Gericht(Gerichtlist.length, $scope.NameIn, arr[0], arr[1], $scope.BeschreibungIn, $scope.DauerIn, ImgDATAuri, KategorieIn);
     ResetInput();
-    rezept.addRezept(GerichtNew);
+    console.log(GerichtNew);
+}
 
+function StrToArr(str)
+{
+    str[0].replace(', ', ',');
+    str[0].replace(' ,', ',');
+    str[1].replace('. ', ',');
+    str[1].replace(' .', ',');
+    // Ausfteilung der einzelnen Zutaten / Zubereitungsschritte
+    str[0] = str[0].split(',');
+    str[1] = str[1].split('.');
+    return str;
 }
 
 var ImgDATAuri = "";
@@ -14,16 +29,18 @@ var ImgDATAuri = "";
 function SetImgDATAuri(DATAuri)
 {
     ImgDATAuri = DATAuri;
-    alert("var is set");
+    document.getElementById("AddImgSuccess").className = "bar bar-header bar-balanced";
+    $("#AddImgSuccess").fadeIn();
+    $("#AddImgSuccess").delay(5000).fadeOut();
 }
 
 function ResetInput()
 {
     document.getElementById("AddRezeptSuccess").className = "bar bar-header bar-balanced";
-    //document.getElementById("AddRezeptSuccess").delay(5000).fadeOut();
+    $("#AddRezeptSuccess").fadeIn();
+    $("#AddRezeptSuccess").delay(5000).fadeOut();
     for (var i = 1; i < 6; i++)
     {
-        alert("Input" + i);
         document.getElementById("Input" + i).value = "";
     }
 }
--- App/www/js/controllers.js
+++ App/www/js/controllers.js
@@ -7,22 +7,21 @@ angular.module('starter.controllers', [])
         })
 
         .controller('RezepteCtrl', function ($scope, rezepte, $stateParams) {
-            $scope.Rezepte = rezepte.all();
-            $scope.rezept = rezepte.get($stateParams.rezeptId);
-            $scope.Kategorien = rezepte.getKat();
             $scope.Rezepte = rezepte.all(); // -- Rezepte Array
             $scope.rezept = rezepte.get($stateParams.rezeptId); // -- Rezept als Objekt
             $scope.Kategorien = rezepte.getKat(); // -- Kategorie Array
             $scope.query = {name: ""}; // -- Filterfunktion
             $scope.sort = {predicate: "name", reverse: false}; // -- Filterfunktion
+            console.log($scope.rezept);
+            console.log("test");
         })
 
 
 
         .controller('AddCtrl', function ($scope) {
-            $scope.add = function (){
+            $scope.add = function () {
                 addR($scope);
-                
+
             };
         })
 
@@ -36,24 +35,46 @@ angular.module('starter.controllers', [])
 
             $scope.takePicture = function () {
                 var options = {
-                    quality: 75,
+                    quality: 95,
                     destinationType: Camera.DestinationType.DATA_URL,
                     sourceType: Camera.PictureSourceType.CAMERA,
                     allowEdit: true,
                     encodingType: Camera.EncodingType.JPEG,
-                    targetWidth: 300,
-                    targetHeight: 300,
+                    targetWidth: 800,
+                    targetHeight: 800,
                     popoverOptions: CameraPopoverOptions,
                     saveToPhotoAlbum: true
                 };
 
                 $cordovaCamera.getPicture(options).then(function (imageData) {
                     $scope.imgURI = "data:image/jpeg;base64," + imageData;
-                    alert("done coding");
                     SetImgDATAuri($scope.imgURI);
-                    alert($scope.imgURI);
                 }, function (err) {
                     alert(err);
+                });
+            };
+
+        })
+        .controller('ImagePickerController', function ($scope, $cordovaImagePicker) {
+            $scope.getImageSaveContact = function () {
+                // Image picker will load images according to these settings
+                var options = {
+                    maximumImagesCount: 1, // Max number of selected images, I'm using only one for this example
+                    width: 800,
+                    height: 800,
+                    quality: 95,
+                    encodingType: Camera.EncodingType.JPEG// Higher is better
+                };
+
+                $cordovaImagePicker.getPictures(options).then(function (results) {
+                    // Loop through acquired images
+                        var imageData=$base64.encode(results);
+                        $scope.imgURI = "data:image/jpeg;base64," + imageData;
+                        SetImgDATAuri($scope.imgURI);
+                        alert($scope.imgURI);// Print image URI
+                    
+                }, function (error) {
+                    alert (error);  // In case of error
                 });
             };
 
